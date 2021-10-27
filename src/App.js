7a3a9142891df508bc391ac92904cdc4dc3ed259
Robert Mengual <robert.mengual@outlook.com>
2021-10-27 15:30:31 +0200

    Test load restaurants action

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Provider } from 'react-redux';
+import store from './store';
 import RestaurantScreen from './components/RestaurantScreen';
 
 const App = () => {
-  return <RestaurantScreen />;
+  return (
+    <Provider store={store}>
+      <RestaurantScreen />
+    </Provider>
+  );
 };
 
 export default App;
