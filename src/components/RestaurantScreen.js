a9b858e25f00b50b190d42385efd8401eeb280d1
Robert Mengual <robert.mengual@outlook.com>
2021-10-28 15:54:17 +0200

    update styles

--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -1,11 +1,14 @@
+import { Card, CardContent, Typography } from '@material-ui/core';
 import RestaurantList from './RestaurantList';
 
 const RestaurantScreen = () => {
   return (
-    <div>
-      <h1>Restaurants</h1>
-      <RestaurantList />
-    </div>
+    <Card>
+      <CardContent>
+        <Typography variant="h5">Restaurants</Typography>
+        <RestaurantList />
+      </CardContent>
+    </Card>
   );
 };
 
